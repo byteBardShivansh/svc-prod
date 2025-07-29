90f03815f485d6584f7afa403f0994d40e6b158b
Shivansh Mishra <autocodebuddy@gmail.com>
2025-07-29 20:13:06 +0530

    Random names for buckets

--- main.tf
+++ main.tf
@@ -1,9 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
 provider "google" {
   project = "sound-habitat-462410-m4"
   region  = "us-central1"
 }
 
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
 resource "google_storage_bucket" "web_server_logs" {
-  name     = "web-server-logs"
+  name     = "web-server-logs-${random_id.bucket_suffix.hex}"
   location = "US"
-} 
+}
