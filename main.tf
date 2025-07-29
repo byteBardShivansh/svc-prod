e8f297c70271dc2ff80af38b94e8ab3929c08151
Shivansh Mishra <autocodebuddy@gmail.com>
2025-07-29 20:49:57 +0530

    added Versioning and added terraform destroyed

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5"
-    }
-  }
-}
-
 provider "google" {
   project = "sound-habitat-462410-m4"
   region  = "us-central1"
@@ -23,4 +10,8 @@ resource "random_id" "bucket_suffix" {
 resource "google_storage_bucket" "web_server_logs" {
   name     = "web-server-logs-${random_id.bucket_suffix.hex}"
   location = "US"
+
+  versioning {
+    enabled = true
+  }
 }
